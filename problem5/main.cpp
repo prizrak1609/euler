fe737766486b3ff02fabb642c53a47d4be23a0d1
Dima <gubatenkod@icloud.com>
2017-12-24 04:27:47 +0200

    problem 5

---
+++ problem5/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int nod(const int first, const int second)
+{
+    int nod = first;
+    int tmp = second;
+    while (tmp != 0 && nod != 0)
+    {
+        if (tmp > nod)
+        {
+            tmp %= nod;
+        }
+        else
+        {
+            nod %= tmp;
+        }
+    }
+    if (nod == 0)
+    {
+        nod = tmp;
+    }
+    return nod;
+}
+
+int nok(const int first, const int second)
+{
+    int _nod = nod(first, second);
+    if (_nod != 0)
+    {
+        return second * first / _nod;
+    }
+    return 0;
+}
+
+int main()
+{
+    int lastNumber = 1;
+    long int number = 2;
+    for (int i = 2; i<=20; i++)
+    {
+        number = nok(number, lastNumber);
+        lastNumber = i;
+    }
+    std::cout << number << std::endl;
+    return 0;
+}
